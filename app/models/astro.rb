37707b4f0756d3a419915cf16b9de3f4eea82283
Ben Yellin <byellindc@gmail.com>
2019-02-11 15:59:43 -0500

    updated user and astro model

--- app/models/astro.rb
+++ app/models/astro.rb
@@ -5,4 +5,36 @@ class Astro < ApplicationRecord
   validates :date_start, presence: true
   validates :date_end, presence: true
   validates :color, presence: true
+
+  alias_attribute :name, :sign
+
+  def start_date
+    Date.parse(self.date_start)
+  end
+
+  def end_date
+    Date.parse(self.date_end)
+  end
+
+  def date_within?(date)
+    # ensures date year is current year
+    date = Date.parse(date.strftime('%b %d'))
+
+    if self.sign == 'Capricorn'
+      # capricorn spans between two years
+
+      # is date in december?
+      if date >= self.start_date
+        date = date.last_year
+      end
+
+      date.between?(self.start_date.last_year, self.end_date)
+    else
+      date.between?(self.start_date, self.end_date)
+    end
+  end
+
+  def user_within?(user)
+    self.date_within?(user.birthday)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,29 @@ class User < ApplicationRecord
   has_many :matched, class_name: "Match", foreign_key: "user2_id", dependent: :destroy
   belongs_to :astro, optional: true
 
+  after_create :assign_astrological_sign
+
   validates :name, presence: true
-  validates :username, presence: true, format: { without: /\s/ }
-  validates :email, presence: true, format: { with: /\A[^\s]*@[^\s]*\Z/ }
+  validates :username, presence: true, uniqueness: true, format: { without: /\s/ }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^\s]*@[^\s]*\Z/ }
   validates :birthday, presence: true
   validate :is_legal_age
 
+  def current_birthday
+    Date.parse(self.birthday.strftime('%B %d'))
+  end
+
+  private 
+
   def is_legal_age
     self.birthday <= 18.years.ago
+  end
+
+  def find_astrological_sign
+    Astro.find { |astro| astro.user_within?(self) }
+  end
+
+  def assign_astrological_sign
+    self.astro = find_astrological_sign
   end
 end
