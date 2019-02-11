778607c24a04f2698f8b7eb0be9e2a7f3552b713
Ben Yellin <byellindc@gmail.com>
2019-02-11 12:33:06 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :compatabilities
-  resources :astros
-  resources :users
+  resources :astros, only: %i(index show)
+  resources :users, except: %i(index)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
