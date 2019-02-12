559dffdaa0c96aa206945cd51777e704df5ea3c5
Ross Jones <rossmosis.jones@gmail.com>
2019-02-12 17:56:30 -0500

    added user authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  helper_method :logged_in?
+
+  def current_user
+    if @user
+      return @user
+    else
+      @user = User.find_by(id: session["user_id"])
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    flash["notice"] = "You must be logged in to view that page."
+    redirect_to login_path unless logged_in?
+  end
 end
