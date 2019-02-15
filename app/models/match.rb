6c75c6f47704ded32bb7a9a912e5a7941c8fa1c9
Ben Yellin <byellindc@gmail.com>
2019-02-15 03:09:31 -0500

    updated match card avatar

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,12 +2,12 @@ class Match < ActiveRecord::Base
   belongs_to :compatability
   belongs_to :user1, class_name: "User", foreign_key: "user1_id"
   belongs_to :user2, class_name: "User", foreign_key: "user2_id"
-  delegate :score, to: :compatability
-
+  
   alias_attribute :user, :user2  
-
-  delegate :name, :username, :astro_sign, :photo_filename, to: :user, prefix: true
-  # delegate :name, :username, :photo_filename, to: :user2, prefix: true
+  
+  delegate :score, to: :compatability
+  delegate :name, :username, :astro_sign, to: :user, prefix: true
+  delegate :avatar_url, :card_avatar_url, to: :user
 
   def score_string
     "#{score}% Match"
